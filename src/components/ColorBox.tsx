3fb861013de54f75c725f6032889343167f6a922
Arzivre <sonyfauzi@outlook.co.id>
2022-10-05 17:32:48 +0700

    add useClipboard

--- src/components/ColorBox.tsx
+++ src/components/ColorBox.tsx
@@ -1,13 +1,47 @@
-import { Color } from '../seedColors'
+import { useState } from 'react'
+
+function useClipboard({ timeout = 2000 } = {}) {
+  const [error, setError] = useState<Error | null>(null)
+  const [copied, setCopied] = useState(false)
+  const [copyTimeout, setCopyTimeout] = useState<number | undefined>()
+
+  const handleCopyResult = (value: boolean) => {
+    clearTimeout(copyTimeout)
+    setCopyTimeout(setTimeout(() => setCopied(false), timeout))
+    setCopied(value)
+  }
+
+  const copy = (valueToCopy: any) => {
+    if ('clipboard' in navigator) {
+      navigator.clipboard
+        .writeText(valueToCopy)
+        .then(() => handleCopyResult(true))
+        .catch((err) => setError(err))
+    } else {
+      setError(new Error('useClipboard: navigator.clipboard is not supported'))
+    }
+  }
+
+  const reset = () => {
+    setCopied(false)
+    setError(null)
+    clearTimeout(copyTimeout)
+  }
+
+  return { copy, reset, error, copied }
+}
 
 interface ColorBoxPageProps {
   background: string
   name: string
 }
 const ColorBox = ({ background, name }: ColorBoxPageProps) => {
+  const clipboard = useClipboard()
+
   return (
     <div
       style={{ background }}
+      onClick={() => clipboard.copy(background)}
       className='group relative my-0 mx-auto -mb-[7px] inline-block h-[25%] 
       w-[20%] cursor-pointer'
     >
@@ -16,16 +50,17 @@ const ColorBox = ({ background, name }: ColorBoxPageProps) => {
           <span>{name}</span>
         </div>
         <button
+          onClick={() => clipboard.copy(background)}
           className='copy-button absolute  top-[50%] left-[50%] -mt-[15px] -ml-[50px] block
           h-[30px] w-[100px] bg-[rgba(255,255,255,0.3)] text-center text-base uppercase text-white
           opacity-0 outline-none group-hover:opacity-100 group-hover:transition group-hover:delay-100'
         >
-          Copy
+          {clipboard.copied ? 'Copied' : 'Copy'}
         </button>
       </div>
       <span
         className='absolute right-0 bottom-0 w-16 bg-[rgba(255,255,255,0.3)] 
-        text-center leading-8 text-white uppercase'
+        text-center uppercase leading-8 text-white'
       >
         More
       </span>
