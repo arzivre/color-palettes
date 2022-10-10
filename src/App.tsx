b9ee4ea70741585c1c72ef1610a4e69ad40e4873
Arzivre <sonyfauzi@outlook.co.id>
2022-10-10 15:42:07 +0700

    add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import Palette from './components/Palette'
 import seedColors from './seedColors'
 import { generatePalette } from './colorHelpers'
+import { Routes, Route } from 'react-router-dom'
 
 export default function App() {
   return (
-    <div>
-      <Palette generatedPalette={generatePalette(seedColors[1])} />
-    </div>
+    <Routes>
+      <Route
+        path='/'
+        element={<Palette generatedPalette={generatePalette(seedColors[1])} />}
+      />
+      <Route
+        path='/palette/:id'
+        element={<Palette generatedPalette={generatePalette(seedColors[1])} />}
+      />
+    </Routes>
   )
 }
