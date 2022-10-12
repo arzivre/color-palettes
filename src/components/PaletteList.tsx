251b845eff7740bfb7919b13d7720568f73ecd8f
Arzivre <sonyfauzi@outlook.co.id>
2022-10-12 11:34:17 +0700

    finish styling PaletteMini

--- src/components/PaletteList.tsx
+++ src/components/PaletteList.tsx
@@ -4,10 +4,10 @@ import PaletteMini from './PaletteMini'
 
 const PaletteList = () => {
   return (
-    <div className='flex h-full items-start justify-center bg-blue-600'>
+    <div className='h-[110vh] flex items-start justify-center bg-blue-600'>
       <div className='flex w-[60%] flex-col flex-wrap items-start'>
         <nav className='flex w-full justify-between text-white'>
-          <h1>React Pallettes</h1>
+          <h1 className='my-4 text-xl font-semibold'>React Color Pallettes</h1>
         </nav>
 
         <ol className='border-box grid w-full grid-cols-3 gap-[5%]'>
--- src/components/PaletteMini.tsx
+++ src/components/PaletteMini.tsx
@@ -1,14 +1,22 @@
 import { Palette } from '../seedColors'
 
 const PaletteMini = (props: Palette) => {
-  const { paletteName, emoji } = props
+  const { paletteName, emoji, colors } = props
+
+  const colorBoxesMini = colors.map((color) => (
+    <div
+      key={color.name}
+      style={{ backgroundColor: color.color }}
+      className='relative my-0 mx-auto -mb-1.5 inline-block h-[25%] w-[20%]'
+    />
+  ))
 
   return (
     <div
       className='relative overflow-hidden rounded-md border-[1px]
       border-black bg-white p-2 hover:cursor-pointer'
     >
-      <div className='bg-gray-300'></div>
+      <ol className='h-36 w-full overflow-hidden rounded'>{colorBoxesMini}</ol>
       <h5
         className='relative m-0 flex items-center justify-between pt-2 
         text-base text-black'
