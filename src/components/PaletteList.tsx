d5bd51b3130b1343183014cc8f423ff954cef46a
Arzivre <sonyfauzi@outlook.co.id>
2022-10-12 11:18:02 +0700

    start styling PaletteList

--- src/components/PaletteList.tsx
+++ src/components/PaletteList.tsx
@@ -4,15 +4,20 @@ import PaletteMini from './PaletteMini'
 
 const PaletteList = () => {
   return (
-    <div>
-      <h1>React Pallettes</h1>
-      <ol>
-        {seedColors.map((palette) => (
-          <li key={palette.id}>
-            <PaletteMini {...palette} />
-          </li>
-        ))}
-      </ol>
+    <div className='flex h-full items-start justify-center bg-blue-600'>
+      <div className='flex w-[60%] flex-col flex-wrap items-start'>
+        <nav className='flex w-full justify-between text-white'>
+          <h1>React Pallettes</h1>
+        </nav>
+
+        <ol className='border-box grid w-full grid-cols-3 gap-[5%]'>
+          {seedColors.map((palette) => (
+            <li key={palette.id}>
+              <PaletteMini {...palette} />
+            </li>
+          ))}
+        </ol>
+      </div>
     </div>
   )
 }
