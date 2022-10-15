0c45d3612cf6ded43a0a8dd3cdd503fd7696ae09
Arzivre <sonyfauzi@outlook.co.id>
2022-10-15 15:22:52 +0700

    add code spliting into route

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,28 @@
-import Palette from './components/Palette'
-import { Routes, Route, useParams } from 'react-router-dom'
-import PaletteList from './components/PaletteList'
-import PaletteSingleColor from './components/PaletteSingleColor'
+import { lazy, Suspense } from 'react'
+import { Route, Routes } from 'react-router-dom'
+
+const PaletteList = lazy(() => import('./components/PaletteList'))
+const PaletteSingleColor = lazy(() => import('./components/PaletteSingleColor'))
+const Palette = lazy(() => import('./components/Palette'))
+
+const loading = (
+  <p className='align-center h-[100vh] justify-center text-center'>
+    Loading ...
+  </p>
+)
 
 export default function App() {
   return (
-    <Routes>
-      <Route path='/' element={<PaletteList />} />
-      <Route path='/palette/:id' element={<Palette />} />
-      <Route
-        path='/palette/:paletteId/:colorId'
-        element={<PaletteSingleColor />}
-      />
-    </Routes>
+    <Suspense fallback={loading}>
+      <Routes>
+        <Route path='/' element={<PaletteList />} />
+        <Route path='/palette/new' element={<h1>New Palette Form</h1>} />
+        <Route path='/palette/:id' element={<Palette />} />
+        <Route
+          path='/palette/:paletteId/:colorId'
+          element={<PaletteSingleColor />}
+        />
+      </Routes>
+    </Suspense>
   )
 }
